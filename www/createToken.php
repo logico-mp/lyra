9cf902161edae5a60aa43bd887aeb0906bcecc56
Luciano Verni <lucianotv12@gmail.com>
2023-06-30 17:22:41 -0300

    s

--- www/createToken.php
+++ www/createToken.php
@@ -24,13 +24,13 @@ if (isset($_GET['requestObject'])) {
                              "billingDetails" => array(
                                  "firstName" => $_usuario->get_nombre(),
                                  "lastName" => $_usuario->get_apellido(),
-                                 "phoneNumber" => $compra->idDomicilio->telefono,
-
-                                 "streetNumber" => is_string( str_replace(array(',', '.'), '', $compra->idDomicilio->numero)),
-                                 "address" => is_string( str_replace(array(',', '.'), '', $compra->idDomicilio->calle)),
-                                 "zipCode" => is_string($compra->idDomicilio->cp),
-                                 "city" => is_string($compra->idDomicilio->localidad),
-                                 "state" => $compra->idDomicilio->provinciaId->nombre,
+//                                 "phoneNumber" => $compra->idDomicilio->telefono,
+//
+//                                 "streetNumber" => is_string( str_replace(array(',', '.'), '', $compra->idDomicilio->numero)),
+//                                 "address" => is_string( str_replace(array(',', '.'), '', $compra->idDomicilio->calle)),
+//                                 "zipCode" => is_string($compra->idDomicilio->cp),
+//                                 "city" => is_string($compra->idDomicilio->localidad),
+//                                 "state" => $compra->idDomicilio->provinciaId->nombre,
                                  "identityCode" => $_usuario->get_id()
                              ),
                         "reference" => $compra->get_id()                            
