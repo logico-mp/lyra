7804d50b68b60492bb76144e021100082366be28
Luciano Verni <lverni@soylogico.com>
2025-02-14 11:17:17 -0300

    s

--- www/createToken.php
+++ www/createToken.php
@@ -37,7 +37,7 @@ if (isset($_GET['requestObject'])) {
                     "orderId" => uniqid($compra->get_id()),
                     "transactionOptions" => array(
                         "cardOptions" => array(
-                            "restrictedInstallments" =>  array(1, 3)
+                            "restrictedInstallments" =>  array(1, 3,8)
                         )
                     ),
                     "strongAuthentication" => "CHALLENGE_MANDATE"
